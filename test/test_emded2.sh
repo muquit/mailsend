2387f6b092571737ea1bf1d193594ce895da7e10
muquit <muquit@gmail.com>
2014-01-17 17:06:29 -0500

    commit

---
+++ test/test_emded2.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+#
+# muquit@muquit.com Dec-27-2013
+TO=$TO
+if [ x"$TO" = x ]; then
+    echo "TO environment variable is not not"
+    exit 1
+fi
+FROM=$FROM
+if [ x"$FROM" = x ]; then
+    echo "FROM environment variable is not not"
+    exit 1
+fi
+
+pass=$SMTP_USER_PASS
+if [ x"$pass" = x ]; then
+    echo "SMTP_USER_PASS environment variable is not not"
+    exit 1
+fi
+
+./mailsend -v -sub "Embed images in a HTML table. Uses explicit Conteny-Type header" \
+        -from $FROM -to $TO \
+        -smtp smtp.gmail.com -port 587 -starttls -auth \
+        -user $FROM -cs ISO-8859-1 \
+        -content-type "multipart/related" \
+        -mime-type text/html \
+        -disposition inline -enc-type "none" \
+        -attach "/home/muquit/svndev/mailsend/test/embed_image2.html" \
+        -mime-type "image/png" -enc-type "base64" \
+        -disposition inline \
+        -content-id "img1_deadbeefcafe" -cs "none" \
+        -attach "$HOME/test_images/test.jpg" \
+        -content-id "img2_deadbeefcafe" \
+        -attach "$HOME/test_images/testpattern001.png"
---
+++ test/test_emded3.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#
+# muquit@muquit.com Dec-27-2013
+set -x
+
+TO=$TO
+if [ x"$TO" = x ]; then
+    echo "TO environment variable is not not"
+    exit 1
+fi
+FROM=$FROM
+if [ x"$FROM" = x ]; then
+    echo "FROM environment variable is not not"
+    exit 1
+fi
+
+pass=$SMTP_USER_PASS
+if [ x"$pass" = x ]; then
+    echo "SMTP_USER_PASS environment variable is not not"
+    exit 1
+fi
+
+./mailsend -sub "Embed 2 images in HTML, one below the other" \
+        -from $FROM -to $TO \
+        -smtp smtp.gmail.com -port 587 -starttls -auth \
+        -user $FROM -cs ISO-8859-1 \
+        -embed-image "$HOME/test_images/test.jpg" \
+        -embed-image "$HOME/test_images/testpattern001.png"
