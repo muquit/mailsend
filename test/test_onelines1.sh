15acd1dc30ffc5bbc2cccedd66ab775b74fbac3a
m <muquit@t105>
2014-02-01 19:58:58 -0500

    add test script for -M

---
+++ test/test_onelines1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# test online messages with -M
+# Issue# 45
+# muquit@muquit.com Feb-01-2014 
+TO=$TO
+if [ x"$TO" = x ]; then
+    echo "TO environment variable is not not"
+    exit 1
+fi
+FROM=$FROM
+if [ x"$FROM" = x ]; then
+    echo "FROM environment variable is not not"
+    exit 1
+fi
+
+pass=$SMTP_USER_PASS
+if [ x"$pass" = x ]; then
+    echo "SMTP_USER_PASS environment variable is not not"
+    exit 1
+fi
+
+./mailsend -v -sub "testing oneline messages no encoding type" \
+        -from $FROM -to $TO \
+        -smtp smtp.gmail.com -port 587 -starttls -auth \
+        -user $FROM \
+        -M "one line message 1" \
+        -M "one line message 2"
