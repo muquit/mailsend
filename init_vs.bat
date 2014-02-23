e3f74943b0d3bf9f374dae6291fb571a37c053ef
m <muquit@t105>
2014-02-23 16:51:14 -0500

    bat file to initialize vs

---
+++ init_vs.bat
@@ -0,0 +1,5 @@
+REM your env could be different
+@echo %VS100COMMONTOOLS%
+REM 
+REM "%VS100COMMONTOOLS%\vsvars32.bat
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
